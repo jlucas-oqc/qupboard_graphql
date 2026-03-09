7c9bae86a0e8f3778123f292d7c2b6b1de208da1
jlucas-oqc <jlucas@oqc.tech>
2026-03-09 11:45:46 +1100

    docs: remove sites

--- site/assets/javascripts/lunr/tinyseg.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:83ba7fd6a6b7cfb2fa8b654fdbe06e22d25fa68a2ba7d8187f8b59f086716950
-size 22865
--- site/assets/javascripts/lunr/wordcut.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d2ba824bb95bdf1d9debc0251d615c3b7e0e55b7b2c83704a990efaad114a0e6
-size 677218
